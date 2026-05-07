3832bc34786994b4f6c6f2251debf93dcf110790
Vrushali <vrushalibhise16@gmail.com>
2026-05-07 17:10:07 +0530

    Created CustomeeDAO and customerDBModel for requirement 13

---
+++ PhoniexTestAutomationFramework/src/test/java/com/db/model/DemoRunner.java
@@ -0,0 +1,15 @@
+package com.db.model;
+
+public class DemoRunner {
+
+	public static void main(String[] args) {
+		CustomerDBModel customer = new CustomerDBModel("Vrushali", "Bhise", "45653456786", "", 
+				"vrush@gmail.com", "");
+		
+		System.out.println(customer);
+		
+		
+
+	}
+
+}
